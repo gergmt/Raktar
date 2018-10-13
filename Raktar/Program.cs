ad732ad0a7fab2f21538b06bceb362f09cd81272
Gonda Gergő <kisgonda@gmail.com>
2018-10-13 15:07:55 +0200

    Menü 0.1

--- Raktar/Program.cs
+++ Raktar/Program.cs
@@ -8,8 +8,30 @@ namespace Raktar
 {
     class Program
     {
+        static void Torles()
+        {
+
+        }
+        static void Modositas()
+        {
+
+        }
+        static void Letrehozas()
+        {
+
+        }
+        static void Kereses()
+        {
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Kérem üsse be a parancs kódját");
+            Console.WriteLine("1.Törlés");
+            Console.WriteLine("2.Módosítás");
+            Console.WriteLine("3.Létrehozás");
+            Console.WriteLine("4.Keresés");
+            int valasztas = int.Parse(Console.ReadLine());
         }
     }
 }
